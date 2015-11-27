4b9229ec2a1635849a30dc2bf5eb3532bac4bb2c
Yao Yuan <opal_obsidian@hotmail.com>
2015-11-27 22:28:09 +0000

    first commit

---
+++ com/igrecs/tictactoecontroller/GameController.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import ..tictactoemodels.Player;
+
+public class GameController {
+  private List<Player> players = new ArrayList<Player>();
+
+  public void initGame(){
+    players.add(new Player('O'));
+    players.add(new Player('X'));
+
+    System.out.println("player1 is: " + players.get(0).getPlayerMarker());
+    System.out.println("player2 is: " + players.get(1).getPlayerMarker());
+
+  }
+
+  public static void main(String[] args){
+    GameController game = new GameController();
+    game.initGame();
+  }
+}
---
+++ com/igrecs/tictactoemodels/Player.java
@@ -0,0 +1,17 @@
+package com.igrecs.tictactoemodels;
+
+public class Player {
+  private char playerMarker;
+
+  public Player (char marker) {
+    playerMarker = marker;
+  }
+
+  public char getPlayerMarker(){
+    return playerMarker;
+  }
+
+  // public makeMove(Board board){
+
+  // }
+}
